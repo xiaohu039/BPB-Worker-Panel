547e16fd914d0efcfa34ad885b7133fed775d218
xiaohu039 <yfgzs5039@gmail.com>
2024-07-24 17:46:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,7 @@ let userID = 'c9ea095c-ba5b-41c4-8a1b-ca06df23bac8';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['192.18.134.22'];// ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs= ['47.242.60.161'];// ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
